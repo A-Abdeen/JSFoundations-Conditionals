45eb294de1a96b7f5d3c46973042b2e502adef55
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-10 20:46:05 +0300

    Conditionals done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 // Define the numbers and the operation here
+let num1=parseInt(prompt("Insert first number: "));
+let num2=parseInt(prompt("Insert second number: "));
+let operation=prompt("Is it + , - , * or / ? ")
 
 // Write if-statements here
+if(operation==="+") console.log(num1+num2)
+else if(operation==="-") console.log(num1-num2)
+else if(operation==="*") console.log(num1*num2)
+else if(operation==="/") console.log(num1/num2)
+else console.log("Wrong operation, please review")
